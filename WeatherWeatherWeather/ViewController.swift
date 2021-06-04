86d169a28dc8cebc8403965ee08a1dd793dd4a57
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-06-04 17:24:43 +0300

    init: start from scratch

---
+++ WeatherWeatherWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherWeatherWeather
+//
+//  Created by Hellizar on 4.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
